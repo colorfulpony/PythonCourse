37d9b2cbb241e2d6b2772d5fd35a064df21a6243
Maxim Chuprynsky <flexywall@gmail.com>
2023-01-25 19:43:57 +0200

    Add table to website

--- WebApp/main.py
+++ WebApp/main.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import pandas
 
-st.title("Grisha")
-st.subheader("ГРІША ЛОХ")
-st.write("""ГРІША ЛОХ
-ГРІША ЛОХ""")
+data = {
+    'Series_1': [1,2,3,5,7],
+    'Series_2': [10,30,40,50],
+}
+
+df = pandas.DataFrame(data)
+
+st.title("Our first streamlit app")
+st.subheader("Introducing to our app")
+st.write("""This is text
+Bla bla bla""")
+st.write(df)
